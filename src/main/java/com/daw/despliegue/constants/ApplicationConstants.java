7a814be44e53143187beb175e23f34b7ffc3490c
gabri <gabrielrl2004@gmail.com>
2024-11-25 20:39:32 +0100

    Constantes básicas

--- src/main/java/com/daw/despliegue/constants/ApplicationConstants.java
+++ src/main/java/com/daw/despliegue/constants/ApplicationConstants.java
@@ -1,5 +1,17 @@
 package com.daw.despliegue.constants;
 
 public class ApplicationConstants {
+    private ApplicationConstants() {}
 
+    // Ruta base de la API
+    public static final String BASE_URL = "/api";
+
+    // Rutas del controlador de Customer
+    public static final String CUSTOMER_BASE_URL = BASE_URL + "/customer/";
+
+    // Rutas del controlador de producto
+    public static final String PRODUCT_BASE_URL = BASE_URL + "/product/";
+
+    // Rutas del controlador de pedido
+    public static final String ORDER_BASE_URL = BASE_URL + "/order/";
 }
