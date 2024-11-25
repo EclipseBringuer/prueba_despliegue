4cd03d8db2bfb25601c76952c5cd227a3c90e7bb
gabri <gabrielrl2004@gmail.com>
2024-11-25 20:39:53 +0100

    Rutas añadidas a los controladores

--- src/main/java/com/daw/despliegue/controller/OrderController.java
+++ src/main/java/com/daw/despliegue/controller/OrderController.java
@@ -1,7 +1,10 @@
 package com.daw.despliegue.controller;
 
+import com.daw.despliegue.constants.ApplicationConstants;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(ApplicationConstants.ORDER_BASE_URL)
 public class OrderController {
 }
