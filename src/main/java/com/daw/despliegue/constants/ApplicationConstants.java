709f38a178d8a47ab8a7469f3cacebc905345f4f
gabri <gabrielrl2004@gmail.com>
2024-11-25 21:42:56 +0100

    Nuevas constantes

--- src/main/java/com/daw/despliegue/constants/ApplicationConstants.java
+++ src/main/java/com/daw/despliegue/constants/ApplicationConstants.java
@@ -1,7 +1,8 @@
 package com.daw.despliegue.constants;
 
 public class ApplicationConstants {
-    private ApplicationConstants() {}
+    private ApplicationConstants() {
+    }
 
     // Ruta base de la API
     public static final String BASE_URL = "/api";
@@ -14,4 +15,19 @@ public class ApplicationConstants {
 
     // Rutas del controlador de pedido
     public static final String ORDER_BASE_URL = BASE_URL + "/order/";
+
+    // Nombres de las tablas de la BD
+    public static final String ORDER_TABLE_NAME = "order";
+    public static final String CUSTOMER_TABLE_NAME = "customer";
+    public static final String ITEM_TABLE_NAME = "item";
+    public static final String PRODUCT_TABLE_NAME = "product";
+
+    // Nombres de columnas usadas en código
+    public static final String CUSTOMER_ID_COLUMN = "customer_id";
+    public static final String ORDER_ID_COLUMN = "order_id";
+    public static final String PRODUCT_ID_COLUMN = "order_id";
+
+    // Nombres de campos en las relaciones de entidades
+    public static final String ORDER_CUSTOMER_RELATIONAL_FIELD_NAME = "customer";
+    public static final String ITEM_ORDER_RELATIONAL_FIELD_NAME = "order";
 }
