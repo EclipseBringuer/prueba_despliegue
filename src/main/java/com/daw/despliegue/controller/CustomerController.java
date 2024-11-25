0609b70ac42810c992371a0ef25cffd067117da7
gabri <gabrielrl2004@gmail.com>
2024-11-25 22:05:29 +0100

    Nuevos métodos e inyección de dependencias

--- src/main/java/com/daw/despliegue/controller/CustomerController.java
+++ src/main/java/com/daw/despliegue/controller/CustomerController.java
@@ -1,10 +1,31 @@
 package com.daw.despliegue.controller;
 
 import com.daw.despliegue.constants.ApplicationConstants;
+import com.daw.despliegue.model.Customer;
+import com.daw.despliegue.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(ApplicationConstants.CUSTOMER_BASE_URL)
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    /**
+     * Método para obtener una listado de clientes
+     */
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        return new ResponseEntity<>(customerService.findAll(), HttpStatus.OK);
+    }
 }
--- src/main/java/com/daw/despliegue/service/CustomerService.java
+++ src/main/java/com/daw/despliegue/service/CustomerService.java
@@ -1,7 +1,29 @@
 package com.daw.despliegue.service;
 
+import com.daw.despliegue.exception.ResourceNotFoundException;
+import com.daw.despliegue.model.Customer;
+import com.daw.despliegue.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public List<Customer> findAll() {
+        var customers = customerRepository.findAll();
+
+        if (customers.isEmpty()) {
+            throw new ResourceNotFoundException("No se han encontrado clientes registrados");
+        }
+        return customers;
+    }
 }
