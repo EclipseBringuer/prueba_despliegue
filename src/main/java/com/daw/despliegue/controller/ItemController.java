51ede42035a9889d8ce24ef1a1f7085083e5cd86
gabri <gabrielrl2004@gmail.com>
2024-11-25 22:05:06 +0100

    Implementación de la lógica para item

---
+++ src/main/java/com/daw/despliegue/controller/ItemController.java
@@ -0,0 +1,10 @@
+package com.daw.despliegue.controller;
+
+import com.daw.despliegue.constants.ApplicationConstants;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(ApplicationConstants.ITEM_BASE_URL)
+public class ItemController {
+}
